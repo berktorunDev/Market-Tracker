14374feb49ec9cd232f26a87ffc9840b7d0683f9
BerkTorun <berktorun.dev@gmail.com>
2023-10-22 03:18:16 +0300

    mapperutil used in service classes

--- src/main/java/com/market/tracker/service/CryptoService.java
+++ src/main/java/com/market/tracker/service/CryptoService.java
@@ -7,24 +7,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.market.tracker.dto.CryptoDTO;
 import com.market.tracker.model.Crypto;
 import com.market.tracker.repository.CryptoRepository;
+import com.market.tracker.util.mapper.MapperUtil;
 
 @Service
 public class CryptoService {
 
     private final CryptoRepository cryptoRepository;
+    private final MapperUtil mapperUtil;
 
-    public CryptoService(CryptoRepository cryptoRepository) {
+    public CryptoService(CryptoRepository cryptoRepository, MapperUtil mapperUtil) {
         this.cryptoRepository = cryptoRepository;
+        this.mapperUtil = mapperUtil;
     }
 
-    public Page<Crypto> getAllCryptos(Pageable pageable) {
-        return cryptoRepository.findAll(pageable);
+    public Page<CryptoDTO> getAllCryptos(Pageable pageable) {
+        Page<Crypto> cryptosPage = cryptoRepository.findAll(pageable);
+        return cryptosPage.map(crypto -> mapperUtil.convertToDTO(crypto, CryptoDTO.class));
     }
 
-    public Optional<Crypto> getCryptoById(Long id) {
-        return cryptoRepository.findById(id);
+    public Optional<CryptoDTO> getCryptoById(Long id) {
+        Optional<Crypto> crypto = cryptoRepository.findById(id);
+        return crypto.map(value -> mapperUtil.convertToDTO(value, CryptoDTO.class));
     }
 
     public void saveCryptos(List<Crypto> cryptos) {
